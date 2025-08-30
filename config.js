25bf3f593b982ec4cf90881c40fbf7b7c2c24f71
thompsontcholvens-beep <thompsontcholvens@gmail.com>
2025-08-29 22:53:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -75,7 +75,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "50935380tr
+DEV: process.env.DEV || "50935380895
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
